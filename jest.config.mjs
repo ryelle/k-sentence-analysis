ba4474760a70c264540d9cf13e381b321aaa715a
Kelly Dwan <ryelle@users.noreply.github.com>
2026-07-21 15:58:57 -0400

    Tools: Update packages

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,15 +1,14 @@
-module.exports = {
-	preset: "ts-jest",
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+	dir: "./",
+});
+
+const customJestConfig = {
 	testEnvironment: "node",
 	roots: ["<rootDir>/src"],
 	testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts", "**/?(*.)+(spec|test).js"],
-	transform: {
-		"^.+\\.ts$": [
-			"ts-jest",
-			{
-				useESM: false,
-			},
-		],
-	},
 	moduleFileExtensions: ["ts", "js", "json"],
 };
+
+export default createJestConfig(customJestConfig);
